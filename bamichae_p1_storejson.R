c747fbdcfc169234aef38c7956c8abda3aa927b8
Ben Michaels <bamichae@buffalo.edu>
2017-06-17 14:14:03 -0400

    deleting tokens

--- bamichae_p1_storejson.R
+++ bamichae_p1_storejson.R
@@ -5,10 +5,10 @@ library(base64enc) #for encoding/decoding data
 library(rjson)  #for converting to json
 library(ROAuth)
 
-api_key <- 'nlczeDhBe4m1WkmTWhGeWL9oS'
-api_secret <- 'AsDWSyzPCZuzx5DTW6XRJoYi8x8qR6kvI4rbWrcqAA7T6MuQGT'
-access_token <- '772218532801220608-FcHWRywnYynnFVIjf0GRe0uBNNfsSsH'
-access_secret <- 'Xsy02Xjg9wK6wDqNFJHbYdao0xvY9y2TnxeOz8aEGikxW'
+api_key <- '###################'
+api_secret <- '###################'
+access_token <- '###################'
+access_secret <- '###################'
 setup_twitter_oauth(api_key,api_secret,access_token,access_secret)  #authorization with appropriate keys
 
 
